924d4bc34c4e156651ab93079704be0aabaa8668
Apoorva Verma <av1019@ic.ac.uk>
2021-02-28 07:51:14 +0000

    Recipe links working, layout of home page updated

--- routes/index.js
+++ routes/index.js
@@ -5,32 +5,53 @@ const request = require("request");
 
 // Home page
 router.get("/", async (req, res) => {
-    res.render('index')
-})
+  res.render("index");
+});
+
+// Home page
+router.get("/recipe/:id", async (req, res) => {
+  res.render("recipe");
+});
 
 // Autocomplete options for search bar
 router.get("/search", async (req, res) => {
   try {
-
-    const url = `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=d374f44f9499470bad8a14160bfcfb9e&query=${req.query.term}&number=${req.query.limit}`
-
-    if (true) {
-      request(url, { json: true }, (err, _, body) => {
-        if (err) {
-          return console.log(err);
-        }
-        console.log(body);
-        res.send(body);
-      });
-    }
-    
+    const url = `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=99c9cad1eb524cf6b73db2fca6d61074&query=${req.query.term}&number=${req.query.limit}`;
+    request(url, { json: true }, (err, _, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(body);
+      res.send(body);
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
   }
 });
 
-// // Find recipes given list of ingredients
-// router
+// Find recipes given list of ingredients
+router.post("/recipes", async (req, res) => {
+  try {
+    const ingredients = req.body.ingredients
+
+    const list = ingredients.map(function (key) {
+      return ("+" + key + ",")        
+    }).join("").slice(1, -1);
+
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=99c9cad1eb524cf6b73db2fca6d61074&ingredients=${list}&number=${req.query.limit}`;
+    request(url, { json: true }, (err, _, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(body);
+      res.send(JSON.stringify(body));
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
--- static/script.js
+++ static/script.js
@@ -1,9 +1,12 @@
-var ingredientsSelected = [];
+var ingredients = { ingredients: [] };
 
-// different fucntions to redirect : .navigate() , .assign()
+// Maybe switch to using axios if time
 
+/*
+Autocomplete function
+Get options for dropdown menu
+*/
 $(document).ready(function () {
-    console.log("running");
   $("#dropdownMenu").autocomplete({
     source: async function (request, response) {
       let data = await fetch(
@@ -12,22 +15,123 @@ $(document).ready(function () {
         .then((results) => results.json())
         .then((results) =>
           results.map((result) => {
-            return { label: result.name, value: result.name };
+            return { value: result.name };
           })
         );
       response(data);
     },
     minLength: 2,
-    select: function(event, ui){
-      console.log(ui.item)
-    }
+    select: function (event, ui) {
+      addIngredient(ui.item.value);
+    },
   });
 });
 
 /*
+Select from dropdown menu
+Add selection to ingredients
+*/
+function addIngredient(name) {
+  /*
+  when option selected:
+  get text from option
+  store that option in ingredients
+  updateRecipes()
+  */
+  ingredients.ingredients.push(name);
+  console.log(JSON.stringify(ingredients));
+  updateIngredients();
+  updateRecipes();
+}
+
+/*
+Delete ingredient on page
+Delete ingredient from ingredients
+*/
+function deleteIngredient(ingredient) {
+  /*
+  when option in ingredients is selected
+  delete from ingredients
+  updateRecipes()
+  */
+
+  console.log("DELETING " + ingredient);
+  var filtered = ingredients.ingredients.filter(function(value, index, arr){ 
+      return value != ingredient;
+  });
+  ingredients.ingredients = filtered;
+  updateIngredients();
+}
+
+/*
+Update recipes
+Render recipes using current ingredients
+*/
+function updateRecipes() {
+  /*
+  live update 
+  when called brings top -- recipes
+  sort - (according to what?)
+  call to backend- list of receipes to display on front
+  */
+  var recipes = null;
+  axios.post("http://localhost:5000/recipes?limit=5", ingredients, JSON).then(
+    (response) => {
+      displayRecipes(response.data);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+function displayRecipes(recipes) {
+  console.log(recipes);
+  $(".recipes").children("div").remove();
+  recipes.forEach((r) => {
+    $(".recipes").append(`<div class="recipe" onClick=goToRecipe(${r.id})>
+                          <h2>${r.title}</h2>
+                          <img src = ${r.image}></img>
+                        </div>`);
+  });
+}
+
+/*
+Update ingredients
+Render ingredients using current ingredients
+*/
+function updateIngredients(name) {
+  $(".ingredients").children("div").remove();
+  ingredients.ingredients.forEach((i) => {
+    $(".ingredients").append(`<div class = "ingredient"> <p class="ing">${i}
+  </p><button class="btn" onClick="deleteIngredient(\'${i}\')"><i class="fa fa-close"></i></button></div>`);
+  });
+  updateRecipes();
+}
+
+function goToRecipe(id){
+  window.location = `http://localhost:5000/recipe/${id}`
+}
+
+/*
 function:
-  autocomplete - get dropdown options from backend
-  select from dropdown - add selected item to ingredientsSelected
-  remove ingredient - remove ingredient from selected items
-  update recipes - show recipes using ingredientsSelected       
+  DONE autocomplete - get dropdown options from backend
+  DONE select from dropdown - add selected item to ingredients
+  DONE remove ingredient - remove ingredient from selected items
+  DONE update recipes - show recipes using ingredients       
+*/
+
+/*
+TODO:
+recipe.html
+
+name
+pic
+
+summary
+
+ingredients
+
+instructions
+
 */
